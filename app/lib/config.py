1a39cced36fddcd1b68ed41a542d64e5f8913f11
廖云富 <yunful@jumei.com>
2019-04-12 19:37:19 +0800

    新增支持文件下载

---
+++ app/lib/config.py
@@ -0,0 +1,10 @@
+
+import random
+
+def request_header(source_url=None):
+    return {
+        'User-Agent': 'GoogleBot',
+        'X-Forwarded-For': '121.168.'+str(random.randint(0,255))+'.' + str(random.randint(0,255))
+    }
+
+proxy = 'http://127.0.0.1:1087'
