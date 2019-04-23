f010a82e38409bae405688c55302d15ad6a21248
廖云富 <yunful@jumei.com>
2019-04-24 00:41:04 +0800

    增加Type

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -5,6 +5,7 @@ import 'egg';
 import ExportAuth from '../../../app/controller/auth';
 import ExportAutomator from '../../../app/controller/automator';
 import ExportBase from '../../../app/controller/base';
+import ExportData from '../../../app/controller/data';
 import ExportDribbble from '../../../app/controller/dribbble';
 import ExportDribbbleAccount from '../../../app/controller/dribbble_account';
 import ExportDribbbleJob from '../../../app/controller/dribbble_job';
@@ -16,6 +17,7 @@ declare module 'egg' {
     auth: ExportAuth;
     automator: ExportAutomator;
     base: ExportBase;
+    data: ExportData;
     dribbble: ExportDribbble;
     dribbbleAccount: ExportDribbbleAccount;
     dribbbleJob: ExportDribbbleJob;
